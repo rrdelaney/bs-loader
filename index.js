f3bc44583ca3f23d4b81794c0a2677501dbb2117
Ryan Delaney <rrdelaney@outlook.com>
2017-03-22 01:57:11 -0700

    Add loader option for module dir

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const path = require('path')
 const { readFile } = require('fs')
 const { execFile } = require('child_process')
+const { getOptions } = require('loader-utils')
 const bsb = require.resolve('bs-platform/bin/bsb')
 
-const getJsFile = resourcePath => {
+const outputDir = 'lib'
+
+const getJsFile = (moduleDir, resourcePath) => {
   const mlFileName = resourcePath.replace(process.cwd(), '')
   const jsFileName = mlFileName.replace(/.(ml|re)$/, '.js')
-  return path.join(process.cwd(), 'lib', 'js', jsFileName)
+  return path.join(process.cwd(), outputDir, moduleDir, jsFileName)
 }
 
 const runBsb = callback => {
@@ -14,9 +17,12 @@ const runBsb = callback => {
 }
 
 module.exports = function () {
+  const options = getOptions(this) || {}
+  const moduleDir = options.module || 'js'
+
   this.addDependency(this.resourcePath + 'i')
   const callback = this.async()
-  const compiledFilePath = getJsFile(this.resourcePath)
+  const compiledFilePath = getJsFile(moduleDir, this.resourcePath)
 
   runBsb((err, res) => {
     if (err) {
