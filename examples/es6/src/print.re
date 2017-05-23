bb10306770e105ea0461d2ba9fc86237f9e04af8
William Huang <will.h86@gmail.com>
2017-05-22 23:57:00 -0700

    upload fix

---
+++ examples/es6/src/print.re
@@ -0,0 +1,8 @@
+let f1 = Fib.fib 10;
+let f2 = Fib.fib 20;
+
+let sum = Add.add f1 f2;
+
+Js.log sum;
+
+let elem = <div />;
---
+++ examples/es6/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/print.re',
+  output: {
+    filename: 'out.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(re|ml)$/,
+        use: [{
+          loader: 'bs-loader',
+          options: {
+            module: 'es6',
+          },
+        }],
+    }]
+  },
+
+  resolve: {
+    extensions: ['.re', '.ml', '.js']
+  }
+}
