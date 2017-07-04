edb7f610453d253f970eca5ed824c4a78b28f49a
Ryan Delaney <rrdelaney@outlook.com>
2017-07-04 10:23:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const getBsbErrorMessages = err => err.match(/File [\s\S]*?:\nError: [\s\S]*?\n/
 
 const getCompiledFile = (moduleDir, path, callback) => {
   runBsb((err, stdout, stderr) => {
-    if (err) return callback(stderr, null, stdout)
+    if (err) return callback(stdout, null)
 
     readFile(path, (err, res) => {
       if (err) {
@@ -68,9 +68,9 @@ module.exports = function loader () {
   const callback = this.async()
   const compiledFilePath = getJsFile(moduleDir, this.resourcePath)
 
-  getCompiledFile(moduleDir, compiledFilePath, (err, res, stdout) => {
+  getCompiledFile(moduleDir, compiledFilePath, (err, res) => {
     if (err) {
-      const errorMessages = getBsbErrorMessages(stdout)
+      const errorMessages = getBsbErrorMessages(err)
 
       if (!errorMessages) {
         if (!(err instanceof Error)) err = new Error(err)
