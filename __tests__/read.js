0df36cc7255f12516652fd71f220ec9d591baefb
Ryan Delaney <rrdelaney@outlook.com>
2017-09-07 17:24:40 -0700

    Add caching to reads

--- __tests__/read.js
+++ __tests__/read.js
@@ -2,6 +2,7 @@ const { readBsConfig, readBsConfigSync } = require('../')
 
 test('Read bsconfig from process.cwd()', () => {
   expect(readBsConfig()).rejects.toBeDefined()
+  expect()
 })
 
 test('Read bsconfig from __dirname', async () => {
@@ -30,4 +31,39 @@ test('Read bsconfig from __dirname synchronously', () => {
     },
     'bsc-flags': ['-bs-super-errors']
   })
+})
+
+test('Cache should return same object for async reads', () => {
+  let bs1 = readBsConfig(__dirname)
+  let bs2 = readBsConfig(__dirname)
+
+  expect(bs1).toBe(bs2)
+})
+
+test('Cache should return same rejected promise', () => {
+  let bs1 = readBsConfig()
+  let bs2 = readBsConfig()
+
+  expect(bs1).toBe(bs2)
+})
+
+test('Cache can be disabled for async reads', () => {
+  let bs1 = readBsConfig(__dirname, false)
+  let bs2 = readBsConfig(__dirname, false)
+
+  expect(bs1).not.toBe(bs2)
+})
+
+test('Cache should return same object for sync reads', () => {
+  let bs1 = readBsConfigSync(__dirname)
+  let bs2 = readBsConfigSync(__dirname)
+
+  expect(bs1).toBe(bs2)
+})
+
+test('Cache can be disabled for sync reads', () => {
+  let bs1 = readBsConfigSync(__dirname, false)
+  let bs2 = readBsConfigSync(__dirname, false)
+
+  expect(bs1).not.toBe(bs2)
 })
--- index.js
+++ index.js
@@ -2,20 +2,36 @@ const { readFile, readFileSync } = require('fs')
 const path = require('path')
 const JSON5 = require('json5')
 
-function readBsConfig(cwd = process.cwd()) {
-  return new Promise((resolve, reject) => {
+let asyncConfigCache = {}
+
+function readBsConfig(cwd = process.cwd(), enableCache = true) {
+  if (asyncConfigCache[cwd] && enableCache) return asyncConfigCache[cwd]
+
+  asyncConfigCache[cwd] = new Promise((resolve, reject) => {
     readFile(path.join(cwd, 'bsconfig.json'), (err, res) => {
       if (err) return reject(err)
 
       resolve(JSON5.parse(res.toString()))
     })
   })
+
+  return asyncConfigCache[cwd]
 }
 
-function readBsConfigSync(cwd = process.cwd()) {
+let syncConfigCache = {}
+
+function readBsConfigSync(cwd = process.cwd(), enableCache = true) {
+  if (syncConfigCache[cwd] && enableCache) return syncConfigCache[cwd]
+
   const content = readFileSync(path.join(cwd, 'bsconfig.json'))
+  syncConfigCache[cwd] = JSON5.parse(content.toString())
 
-  return JSON5.parse(content.toString())
+  return syncConfigCache[cwd]
 }
 
-module.exports = { readBsConfig, readBsConfigSync }
+function clearCache() {
+  asyncConfigCache = {}
+  syncConfigCache = {}
+}
+
+module.exports = { readBsConfig, readBsConfigSync, clearCache }
