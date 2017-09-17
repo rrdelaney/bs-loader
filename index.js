28d9181bf44904c02d0859cbeb4f27a5d4be1c98
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-09-17 18:25:03 +0200

    Wrap command in quotes

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ try {
 }
 
 if(os.platform() === 'darwin') {
-  bsb = `script -q /dev/null ${bsb} -make-world -color`
+  bsb = `script -q /dev/null "${bsb} -make-world -color"`
 } else {
-  bsb = `script --return -qfc ${bsb} -make-world -color /dev/null`
+  bsb = `script --return -qfc "${bsb} -make-world -color" /dev/null`
 }
 
 const outputDir = 'lib'
