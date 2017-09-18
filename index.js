528e00003fe52cfd0b3ad74c00a6a700f5d4c2cc
Ryan Delaney <rrdelaney@outlook.com>
2017-09-18 15:37:09 -0700

    Move this.callback outside of promise, remove `new Promise`

--- index.js
+++ index.js
@@ -90,36 +90,36 @@ const getCompiledFileSync = (moduleDir, path) => {
   return transformSrc(moduleDir, res.toString())
 }
 
-const getBsConfigModuleOptions = buildDir => {
-  return new Promise((resolve, reject) =>
-    readBsConfig(buildDir).then(bsconfig => {
-      if (!bsconfig) {
-        throw new Error(`bsconfig not found in ${buildDir}`)
-      }
+const getBsConfigModuleOptions = buildDir =>
+  readBsConfig(buildDir).then(bsconfig => {
+    if (!bsconfig) {
+      throw new Error(`bsconfig not found in ${buildDir}`)
+    }
 
-      if (!bsconfig['package-specs'] || !bsconfig['package-specs'].length) {
-        resolve({ module: 'js', inSource: false })
-      }
+    if (!bsconfig['package-specs'] || !bsconfig['package-specs'].length) {
+      resolve({ module: 'js', inSource: false })
+    }
 
-      const moduleSpec = bsconfig['package-specs'][0]
-      const moduleDir =
-        typeof moduleSpec === 'string' ? moduleSpec : moduleSpec.module
-      const inSource =
-        typeof moduleSpec === 'string' ? false : moduleSpec['in-source']
-      resolve({ moduleDir, inSource })
-    })
-  )
-}
+    const moduleSpec = bsconfig['package-specs'][0]
+    const moduleDir =
+      typeof moduleSpec === 'string' ? moduleSpec : moduleSpec.module
+    const inSource =
+      typeof moduleSpec === 'string' ? false : moduleSpec['in-source']
+
+    return { moduleDir, inSource }
+  })
 
 module.exports = function loader() {
   const options = getOptions(this) || {}
   const buildDir = options.cwd || CWD
-  const bsconfig = getBsConfigModuleOptions(buildDir).then(bsconfig => {
+  const callback = this.async()
+
+  this.addContextDependency(this.context)
+
+  getBsConfigModuleOptions(buildDir).then(bsconfig => {
     const moduleDir = options.module || bsconfig.moduleDir || 'js'
     const inSourceBuild = options.inSource || bsconfig.inSource || false
 
-    this.addContextDependency(this.context)
-    const callback = this.async()
     const compiledFilePath = getJsFile(
       buildDir,
       moduleDir,
