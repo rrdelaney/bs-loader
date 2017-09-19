2c3194eb2abf645c47ef1e199b6fb0fefc7a87f5
Ryan Delaney <rrdelaney@outlook.com>
2017-09-19 12:05:55 -0700

    Type loader `this`

--- flow-typed/webpack.js
+++ flow-typed/webpack.js
@@ -1,14 +1,12 @@
 // @flow
 
-opaque type WebpackCompilation = { [key: string]: any }
-
 declare module 'webpack' {
   declare export type WebpackLoaderThis = {
     async: () => (err: Error | null, res: string | null) => void,
     context: string,
     addContextDependency: (dep: string) => void,
     resourcePath: string,
-    _compilation: WebpackCompilation,
+    _compilation: { [key: string]: any },
     emitError: (err: Error) => void
   }
 }
