2008d51b06676f0567d851dc1960b9274514dc86
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2018-06-15 14:10:13 -0700

    make bsb-js resilient to _all_ errors

--- packages/bsb-js/utils.js
+++ packages/bsb-js/utils.js
@@ -10,6 +10,7 @@ const getErrorRegex = /(File [\s\S]*?:\r?\n|Fatal )[eE]rror: [\s\S]*?(?=ninja|\r
 const packageNotFoundRegex = /Package not found[\s\S]*?:?\r?\n[\s\S]*?[eE]rror:\s?[\s\S]*/g
 const getSuperErrorRegex = /We've found a bug for you![\s\S]*?(?=ninja: build stopped)/g
 const getWarningRegex = /((File [\s\S]*?Warning.+? \d+:)|Warning number \d+)[\s\S]*?(?=\[\d+\/\d+\]|$)/g
+const errorRegexes = [getSuperErrorRegex, getErrorRegex, packageNotFoundRegex]
 
 function platform() /*: 'darwin' | 'linux' | 'wsl' | null */ {
   const isWSL = () => {
@@ -34,18 +35,20 @@ function transformSrc(
   return src.replace(replacer, '$1$3')
 }
 
+// This function is only ever called if an error has been caught. We try to
+// process said error according to known formats, but we default to what we
+// got as an argument if they don't match. This way we always throw an error,
+// thus avoiding successful builds if something has gone wrong.
 function processBsbError(err /*: Error | string */) {
   if (typeof err === 'string') {
-    return (
-      err.match(getSuperErrorRegex) ||
-      err.match(getErrorRegex) ||
-      err.match(packageNotFoundRegex) ||
-      []
-    ).map(e => new Error(e))
-  } else if (err instanceof Error) {
-    return [err]
+    const errors = errorRegexes
+      .map(r => err.match(r))
+      .reduce((a, s) => a.concat(s), [])
+      .filter(x => x)
+
+    return (errors.length > 0 ? errors : [err]).map(e => new Error(e))
   } else {
-    return []
+    return [err]
   }
 }
 
